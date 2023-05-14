448756cd70e39cc2ab91c86033eae05d8f3cccfe
Marsen <thisismysoul@gmail.com>
2023-05-14 23:25:42 +0800

    gpt: authController

---
+++ src/controllers/authController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+import { authService } from "../services/authService";
+
+export const authController = {
+  async login(req: Request, res: Response) {
+    const { username, password } = req.body;
+    try {
+      const token = await authService.login(username, password);
+      res.json({ token });
+    } catch (error) {
+      res.status(401).json({ message: error.message });
+    }
+  },
+};

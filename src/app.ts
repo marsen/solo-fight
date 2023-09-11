a24c58ec45e66601ad7d53aa05f7d61b217291a0
Marsen <admin@marsen.me>
2023-09-11 20:04:33 +0800

    add logger

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
 import express from "express";
-import router from "./routers/index"
+import router from "./routers/index";
+import logger from "./gcp.logger";
 
 const app = express();
 const port = 3000;
 
 // Middleware 函數，用於在最後面加入日誌
-app.use((req, res, next) => {
+app.use((req, res, next) => {// 添加自定义 metadata 到日志
+  const customMetadata = {
+    customKey: 'customValue',
+  };
+  
+  logger.warn('Req start at: ', customMetadata);
   console.log('Request received at:', new Date());
   console.log('Request method:', req.method);
   console.log('Request URL:', req.url);
@@ -16,6 +22,7 @@ app.use(router)
 
 // Middleware 函數，用於在最後面加入日誌
 app.use((req, res, next) => {
+  logger.error('Req end at: %s', new Date());
   console.log('Response sent at:', new Date());
   console.log('Response status:', res.statusCode);
   console.log('------------------------');
---
+++ src/gcp.logger.ts
@@ -0,0 +1,22 @@
+import { createLogger, format, transports } from 'winston';
+import { LoggingWinston } from '@google-cloud/logging-winston';
+
+const projectId = ''; // 替换为您的GCP项目ID
+
+const logger = createLogger({
+  level: 'info', // 日志级别
+  format: format.combine(
+    format.timestamp(), // 添加时间戳
+    format.errors({ stack: true }), // 添加错误堆栈信息
+    format.json() // 输出日志为 JSON 格式
+  ),
+  transports: [
+    new transports.Console(), // 将日志打印到控制台
+    new LoggingWinston({
+      projectId: projectId, // 设置GCP项目ID
+      keyFilename: './cspkey.json', // 服务帐户密钥文件路径，如果已经配置了凭据文件的环境变量，可以省略
+    }),
+  ],
+});
+
+export default logger;

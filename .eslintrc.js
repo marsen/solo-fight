6e65050d08fd2d28671de63e41b082b0f1599dc3
admin@marsen.me <admin@marsen.me>
2023-11-04 15:04:00 +0800

    Squashed commit of the following:
    commit 6ca7b7f8e3cbda56f57c4f979bb96dc766a82245
    Author: admin@marsen.me <admin@marsen.me>
    Date:   Sat Nov 4 14:58:50 2023 +0800
    
        docs: Remove ESLint configuration and add Microsoft ESLint rules
    
    commit 4ebe2ba52cb52cb76b895cef0542e0136b75d171
    Author: admin@marsen.me <admin@marsen.me>
    Date:   Thu Nov 2 19:59:07 2023 +0800
    
        docs: update settings.json and inversify.config.ts
    
    commit 524c1f4c882e1b6b5935f7aadfcb87476ce10f56
    Author: admin@marsen.me <admin@marsen.me>
    Date:   Thu Nov 2 19:54:42 2023 +0800
    
        docs: ESLint
    
    commit d9912f32fd70eff3d7aaf2fa502d62485bfe8fd6
    Author: Marsen <admin@marsen.me>
    Date:   Sat Oct 7 01:38:34 2023 +0800
    
        feat: fix eslint warning
    
    commit 38461fe503f19338477091509b4683e816c596d1
    Author: Marsen <admin@marsen.me>
    Date:   Fri Oct 6 12:01:27 2023 +0800
    
        feat: eslint . --fix
    
    commit d8aee7a9e44a81fefc7babb734f00ffbe74578bc
    Author: Marsen <admin@marsen.me>
    Date:   Thu Oct 5 20:21:48 2023 +0800
    
        config: add cjs

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    '@microsoft/eslint-config-fast-dna'
+  ],
+  rules: {
+    // 在此添加自定義規則
+  }
+};

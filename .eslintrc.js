32f7dc6245e12fc333e005307e68a048d0a718cb
Marsen <thisismysoul@gmail.com>
2023-11-10 22:24:11 +0800

    feat:Refactor ESLint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,17 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   extends: [
     'plugin:@typescript-eslint/recommended',
-    '@microsoft/eslint-config-fast-dna'
   ],
   rules: {
     // 在此添加自定義規則
-  }
+  },
+  overrides: [
+    {
+      files: [ '*.ts'],
+      parserOptions: {
+        project: './tsconfig.json'
+      },
+      extends: 'standard-with-typescript'
+    }
+  ],
 };

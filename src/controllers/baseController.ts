475318d26c6a10694ab61257db8577e1bf95d2e2
admin@marsen.me <admin@marsen.me>
2023-11-11 19:19:51 +0800

    test: red

--- src/controllers/baseController.ts
+++ src/controllers/baseController.ts
@@ -2,6 +2,6 @@ import { type Request, type Response } from 'express'
 
 export class BaseController {
   public healthCheck (request: Request, response: Response): void {
-    throw new Error('Method not implemented.')
+    response.status(200).json({ status: 'UP' })
   }
 }
--- tests/baseController.test.ts
+++ tests/baseController.test.ts
@@ -2,14 +2,16 @@ import { BaseController } from '../src/controllers/baseController'
 import httpMocks from 'node-mocks-http'
 
 describe('Health Check', () => {
-  it('should return status UP', () => {
+  it('should return status OK', () => {
     const controller = new BaseController()
     const request = httpMocks.createRequest()
     const response = httpMocks.createResponse()
+    // 將 response.status 和 response.json 轉換為 Jest mock 函數
+    response.status = jest.fn().mockReturnThis()
+    response.json = jest.fn()
 
     controller.healthCheck(request, response)
-
     expect(response.status).toHaveBeenCalledWith(200)
-    expect(response.json).toHaveBeenCalledWith({ status: 'UP' })
+    expect(response.json).toHaveBeenCalledWith({ status: 'OK' })
   })
 })

5cf0c3e987d15ab26ba050120d2f4d542b458386
admin@marsen.me <admin@marsen.me>
2023-11-11 19:25:09 +0800

    refactor: Fix request and response variable names

--- src/controllers/baseController.ts
+++ src/controllers/baseController.ts
@@ -1,7 +1,7 @@
 import { type Request, type Response } from 'express'
 
 export class BaseController {
-  public healthCheck (request: Request, response: Response): void {
-    response.status(200).json({ status: 'OK' })
+  public healthCheck (req: Request, res: Response): void {
+    res.status(200).json({ status: 'OK' })
   }
 }

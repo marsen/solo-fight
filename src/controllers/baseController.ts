e7391eb967bf1dbd0a95736d1f0868e1ff8d6e0b
admin@marsen.me <admin@marsen.me>
2023-11-11 19:33:51 +0800

    test: Add timestamp to health check response

--- src/controllers/baseController.ts
+++ src/controllers/baseController.ts
@@ -2,6 +2,6 @@ import { type Request, type Response } from 'express'
 
 export class BaseController {
   public healthCheck (req: Request, res: Response): void {
-    res.status(200).json({ status: 'OK' })
+    res.status(200).json({ status: 'OK', timestamp: new Date().toISOString() })
   }
 }
--- tests/baseController.test.ts
+++ tests/baseController.test.ts
@@ -7,6 +7,9 @@ describe('Health Check', () => {
     const target = new BaseController()
     const req = httpMocks.createRequest()
     const res = httpMocks.createResponse()
+    // Mock the current time
+    const mockDate = new Date('2023-11-11T08:05:47Z')
+    jest.spyOn(global, 'Date').mockImplementation(() => mockDate)
     // 將 response.status 和 response.json 轉換為 Jest mock 函數
     res.status = jest.fn().mockReturnThis()
     res.json = jest.fn()
@@ -14,6 +17,6 @@ describe('Health Check', () => {
     target.healthCheck(req, res)
     // Assert
     expect(res.status).toHaveBeenCalledWith(200)
-    expect(res.json).toHaveBeenCalledWith({ status: 'OK' })
+    expect(res.json).toHaveBeenCalledWith({ status: 'OK', timestamp: mockDate.toISOString() })
   })
 })

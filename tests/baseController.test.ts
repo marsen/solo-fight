875d5ee6e80de546eae33a058662b8769436c179
Marsen <thisismysoul@gmail.com>
2023-11-15 03:13:21 +0800

    refactor : test

--- tests/baseController.test.ts
+++ tests/baseController.test.ts
@@ -1,15 +1,22 @@
 import { BaseController } from '../src/controllers/baseController'
 import httpMocks from 'node-mocks-http'
+import type { MockResponse, MockRequest } from 'node-mocks-http'
+import type { Request, Response } from 'express'
 
 describe('Health Check', () => {
+  let target: BaseController
+  let req: MockRequest<Request>
+  let res: MockResponse<Response>
+  beforeEach(() => {
+    target = new BaseController()
+    req = httpMocks.createRequest()
+    res = httpMocks.createResponse()
+  })
   it('should return status OK', () => {
     // Arrange
-    const target = new BaseController()
-    const req = httpMocks.createRequest()
-    const res = httpMocks.createResponse()
-    // Mock the current time
     const mockDate = new Date('2023-11-11T08:05:47Z')
-    jest.spyOn(global, 'Date').mockImplementation(() => mockDate)
+    jest.spyOn(global, 'Date')
+      .mockImplementation(() => mockDate)
     // 將 response.status 和 response.json 轉換為 Jest mock 函數
     res.status = jest.fn().mockReturnThis()
     res.json = jest.fn()

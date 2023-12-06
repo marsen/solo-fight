a9ce4c3bf458118d2c41971c0b1bbf11415c5e18
admin@marsen.me <admin@marsen.me>
2023-12-06 09:14:49 +0800

    refactor: update logger implementation

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,10 @@ import { type Configuration } from './interfaces/utils/configuration'
 import router from './routers/index'
 import container from './inversify.config'
 import TYPES from './types'
+import { type Logger } from './interfaces/utils/logger'
 
 const cfg = container.get<Configuration>(TYPES.Configuration)
+const logger = container.get<Logger>(TYPES.Logger)
 
 const app = express()
 
@@ -13,5 +15,5 @@ app.use(express.json())
 app.use('/', router)
 
 app.listen(cfg.get('PORT'), () => {
-  console.log(`Server started at http://localhost:${cfg.get('PORT')}`)
+  logger.log('info', `Server started at http://localhost:${cfg.get('PORT')}`)
 })
--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -3,15 +3,13 @@ import { type Configuration } from './interfaces/utils/configuration'
 import { type Logger } from './interfaces/utils/logger'
 import TYPES from './types'
 import EnvConfigService from './utils/env.configuration'
-// import gcpLoggerService from './utils/gcp.logger'
 import diskFileService from './services/fsFileService'
 import { type FileService } from './interfaces/utils/fileService'
-import consoleLoggerService from './utils/console.logger'// you can inject other service
+import loggerWinston from './utils/logger.winston'
 
 const container = new Container()
 container.bind<Configuration>(TYPES.Configuration).to(EnvConfigService)
-// container.bind<Logger>(TYPES.Logger).to(gcpLoggerService)
+container.bind<Logger>(TYPES.Logger).to(loggerWinston)
 container.bind<FileService>(TYPES.FileService).to(diskFileService)
-container.bind<Logger>(TYPES.Logger).to(consoleLoggerService)
 
 export default container
